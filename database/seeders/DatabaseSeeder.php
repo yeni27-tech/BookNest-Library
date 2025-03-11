d7f27fe3be447d79c6c6c32f20ed0b2ad1c800a0
Adang aja <adangnugroho5@gmail.com>
2025-03-11 08:30:10 +0700

    insertUser

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            [
+                'nama_lengkap' => 'Admin',
+                'email' => '',
+                'password' => Hash::make(''),
+                'phone' => '',
+                'address' => '',
+                'user_type' => '',
+                'identity_number' => '',
+                'institution' => '',
+                'library_card_number' => '',
+            ],
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
